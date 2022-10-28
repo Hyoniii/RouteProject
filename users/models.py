05fe4a0029b73f3e9257f0359510ae066ef8676f
hyoniii <phhphh841@hanmail.net>
2022-10-28 23:47:26 +0900

    Add: user models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30, unique=False)
+    email = models.CharField(max_length=50, unique=True)
+
+    nickname = models.CharField(max_length=20, unique=True)
+    profile_image = models.URLField(blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    last_login = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.name + " - " + self.email
+
+
+class UserRouteLike(models.Model):
+    user = models.ForeignKey(User, related_name='users',
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='posts',
+                             on_delete=models.CASCADE)
+
+
+class UserRouteScrap(models.Model):
+    user = models.ForeignKey(User, related_name='users',
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='posts',
+                             on_delete=models.CASCADE)
